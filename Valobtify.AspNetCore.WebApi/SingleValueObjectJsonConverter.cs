1ca5b488a24e38b8ffbf7bdac18bf3899d2815cb
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-11-26 22:51:56 +0330

    chore: update packages

--- Valobtify.AspNetCore.WebApi/SingleValueObjectJsonConverter.cs
+++ Valobtify.AspNetCore.WebApi/SingleValueObjectJsonConverter.cs
@@ -4,21 +4,17 @@ using System.Text.Json.Serialization;
 namespace Valobtify.AspNetCore.WebApi;
 
 public class SingleValueObjectJsonConverter<TSingleValueObject, TValue> : JsonConverter<TSingleValueObject>
-    where TSingleValueObject : SingleValueObject<TSingleValueObject, TValue>, ICreatableValueObject<TSingleValueObject, TValue>
-    where TValue : notnull
+    where TSingleValueObject : SingleValueObject<TSingleValueObject, TValue>,
+    ICreatableValueObject<TSingleValueObject, TValue>
 {
-    public override TSingleValueObject? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override TSingleValueObject? Read(ref Utf8JsonReader reader, Type typeToConvert,
+        JsonSerializerOptions options)
     {
-        TValue? value = JsonSerializer.Deserialize<TValue>(ref reader, options);
-
-        if (value is null) return null;
+        var value = JsonSerializer.Deserialize<TValue>(ref reader, options);
 
         var result = TSingleValueObject.Create(value);
 
-        if (result.IsFailure)
-        {
-            throw new JsonException(result.Errors[0].Message);
-        }
+        if (result.IsFailure) throw new JsonException(result.Errors[0].Message);
 
         return result;
     }
